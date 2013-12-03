876d61d91bbe2c33b9a1482150711f92e72de8be
Collin Simon <cdsimon2@ncsu.edu>
2013-12-03 14:07:21 -0500

    Added the decryption functionality

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import sys
 from os import urandom
 from image import png
 
-def main():
-	if len(sys.argv) != 2:
-		print("ERROR: No filename provided!")
+def encrypt():
+	if len(sys.argv) != 5:
+		print("USAGE: python3 main.py encrypt <input_image> <output_image> <pad_output>")
 		sys.exit(1)
 
-	image = png.parse(sys.argv[1])
+	image = png.parse(sys.argv[2])
 
 	print(image)
 
@@ -35,12 +35,45 @@ def main():
 
 
 
-	image.write_to_file("output.png")
+	image.write_to_file(sys.argv[3])
 
-	f = open('pad.txt', 'wb')
+	f = open(sys.argv[4], 'wb')
 	f.write(pad)
 	f.close()
 
+def decrypt():
+	if len(sys.argv) != 5:
+		print("USAGE: python3 main.py decrypt <input_image> <output_image> <pad_input>")
+		sys.exit(1)
+
+	image = png.parse(sys.argv[2])
+
+	print(image)
+
+	pad = bytearray(open(sys.argv[4], 'rb').read())
+
+	position = 0
+
+	if image.colortype != 2:
+		scanlineLength = image.width + 1
+	else:
+		scanlineLength = image.width * 3 + 1
+
+	for i in image.chunks:
+		if i.type() == 'IDAT':
+			for j in range(len(i.data)):
+				if position % scanlineLength != 0:
+					i.data[j] ^= pad[position]
+				position += 1
+
+
+
+	image.write_to_file(sys.argv[3])
 
 if __name__ == "__main__":
-	main()
+	if sys.argv[1] == 'encrypt':
+		encrypt()
+	elif sys.argv[1] == 'decrypt':
+		decrypt()
+	else:
+		print("USAGE: python3 main.py <encrypt/decrypt> <additional arguments....>")
